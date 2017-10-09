fffec50ea4cbf4b5c885a1ae315b0d7ad1a0a323
matsumura-taichi <hiroto.in.the.cromagnons@gmail.com>
2017-10-09 15:22:33 +0900

    Add tableView

---
+++ GourmetSearch/ShopListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  GourmetSearch
+//
+//  Created by 松村泰地 on 2017/09/30.
+//  Copyright © 2017年 松村泰地. All rights reserved.
+//
+
+import UIKit
+
+class ShopListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 100
+    }
+    
+    //MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ShopListItem") as! ShopListItemTableViewCell
+            cell.name.text = "\(indexPath.row)"
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+}
+
